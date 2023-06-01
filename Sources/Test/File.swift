a4e4c6af7e8e0e37f191d065d81a380df4afa08e
ilikerb0407 <kevin12342247@gmail.com>
2023-06-01 13:47:22 +0800

    Add Test number

---
+++ Sources/Test/File.swift
@@ -0,0 +1,17 @@
+//
+//  File.swift
+//  
+//
+//  Created by TWINB00591630 on 2023/6/1.
+//
+
+import Foundation
+
+public class Test {
+
+    var number: Int = 5
+
+    func printNum() {
+        print(number)
+    }
+}
