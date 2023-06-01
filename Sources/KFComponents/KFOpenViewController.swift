1dd8c30413ac30accc0e896672de7b6ad659478d
ilikerb0407 <kevin12342247@gmail.com>
2023-06-01 10:52:52 +0800

    Test ViewController Access Control

---
+++ Sources/KFComponents/KFOpenViewController.swift
@@ -0,0 +1,29 @@
+//
+//  KFOpenViewController.swift
+//  
+//
+//  Created by TWINB00591630 on 2023/6/1.
+//
+
+import UIKit
+
+open class KFOpenViewController: UIViewController {
+
+    open override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Sources/KFComponents/KFViewController.swift
@@ -0,0 +1,17 @@
+//
+//  KFViewController.swift
+//  
+//
+//  Created by TWINB00591630 on 2023/6/1.
+//
+
+import UIKit
+
+public class KFViewController: UIViewController {
+
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
